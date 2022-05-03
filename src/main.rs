3ad1214922617ef93bfe7af390ef8fec338eb67f
pqx <l.r.slegt@gmail.com>
2022-05-03 03:13:40 +0200

    variables added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+
+
 fn main() {
-    println!("Hello, dcode!");
+    let mut x: u64= 45;
+
+    let f: f32 = 6.7;
+
+    let b: bool = false;
+
+    println!("The value of x is {}", x);
+
+    x = 60;
+
+    println!("The value of x is {}", x);
 }
